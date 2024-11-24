6d6182067dec6e64d1834b867cb1285b4460b70d
EduardoPC05 <epc4129@gmail.com>
2024-11-24 15:41:45 -0300

    feat: criando abstração para actions

---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,18 @@
+package utils;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.dao.PostDao;
+
+import java.io.IOException;
+
+public class Utils {
+
+    public static void viewFeed(HttpServletRequest request, HttpServletResponse response, PostDao postDao) throws ServletException, IOException {
+        request.setAttribute("posts", postDao.getFeed());
+        request.getRequestDispatcher("/src/views/feed.jsp").forward(request, response);
+        System.out.println(postDao.getFeed());
+    }
+
+}
