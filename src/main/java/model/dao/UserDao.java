2e70e3c6f5e64d310d060d8f40e4e37799e2c0c5
LizzBricce <bricce.e@aluno.ifsp.edu.br>
2024-11-24 18:11:59 -0300

    feat: criando metodo para adicionar usuario e fazer login

--- src/main/java/model/dao/UserDao.java
+++ src/main/java/model/dao/UserDao.java
@@ -1,6 +1,8 @@
 package model.dao;
 
 import model.entity.User;
+import utils.PasswordEncoder;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -30,6 +32,7 @@ public class UserDao {
         return Optional.empty();
     }
 
+
     public Optional<User> getUserByUsername(String username) {
         String sql = "SELECT ID, USERNAME, PASSWORD, EMAIL, BIOGRAFY, PATH_PROFILE_PICTURE FROM USER_ARTEMIS WHERE USERNAME = ?";
         try (Connection conn = dataSource.getConnection();
@@ -46,6 +49,36 @@ public class UserDao {
         return Optional.empty();
     }
 
+    public Optional<User> getUserByEmail(String email) {
+        String sql = "SELECT ID, USERNAME, PASSWORD, EMAIL, BIOGRAFY, PATH_PROFILE_PICTURE FROM USER_ARTEMIS WHERE EMAIL = ?";
+        try (Connection conn = dataSource.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, email);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return Optional.of(mapResultSetToUser(rs));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    public Integer getLastUserId() {
+        String sql = "SELECT MAX(ID) FROM USER_ARTEMIS";
+        try (Connection conn = dataSource.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("MAX(ID)");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
     private User mapResultSetToUser(ResultSet rs) throws SQLException {
         User user = new User();
         user.setId(rs.getInt("ID"));
@@ -55,5 +88,54 @@ public class UserDao {
         user.setBiografy(rs.getString("BIOGRAFY"));
         user.setPathProfilePicture(rs.getString("PATH_PROFILE_PICTURE"));
         return user;
+    }
+
+    public Boolean save(User user){
+        Optional<User> optional = getUserByEmail(user.getEmail());
+        if(optional.isPresent()) {
+            return false;
+        }
+
+        String sql = "INSERT INTO USER_ARTEMIS (ID, USERNAME, PASSWORD, EMAIL, BIOGRAFY, PATH_PROFILE_PICTURE) VALUES (? ,?, ?, ?, ?, ?)";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, getLastUserId()+1);
+            ps.setString(2, user.getUsername());
+            ps.setString(3, user.getPassword());
+            ps.setString(4, user.getEmail());
+            ps.setString(5, user.getBiografy());
+            ps.setString(6, user.getPathProfilePicture());
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao inserir no banco", e);
+        }
+        return true;
+    }
+
+    public Optional<User> getUserByEmailAndPassword(String email, String password) {
+        String passwordEncripted = PasswordEncoder.encode(password);
+
+        String sql = "SELECT id, name, email FROM user WHERE email = ? AND password = ?";
+        Optional<User> optional = Optional.empty();
+
+        try (Connection con = dataSource.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, email);
+            ps.setString(2, passwordEncripted);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    User user = new User();
+                    user.setId(rs.getInt(1));
+                    user.setUsername(rs.getString(2));
+                    user.setEmail(rs.getString(3));;
+
+                    optional = Optional.of(user);
+                }
+            }
+            return optional;
+        } catch (SQLException sqlException) {
+            throw new RuntimeException("Erro durante a consulta no BD", sqlException);
+        }
     }
 }
