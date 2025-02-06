779feb0e574bc51d72e99053f8f9676950fbcbe6
LizzBricce <bricce.e@aluno.ifsp.edu.br>
2025-02-06 13:59:51 -0300

    fix: ViewPostsHelper

--- src/main/java/servlets/helpers/ViewPostsHelper.java
+++ src/main/java/servlets/helpers/ViewPostsHelper.java
@@ -1,42 +1,59 @@
 package servlets.helpers;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.dao.PostDao;
-import model.dto.PostListDto;
+import model.dto.PostViewDto;
+import model.entity.Post;
 import model.entity.User;
 import model.mapper.MapperPost;
 import model.utils.Utils;
 import utils.DataSourceSearcher;
 
-import java.util.List;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Optional;
 
 public class ViewPostsHelper implements Helper {
+    private final PostDao postDao;
+    private final MapperPost mapperPost;
+    private final Utils utils;
+
+    public ViewPostsHelper() {
+        this.postDao = new PostDao(DataSourceSearcher.getInstance().getDataSource());
+        this.mapperPost = new MapperPost();
+        this.utils = new Utils();
+    }
+
     @Override
-    public Object execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        PostDao postDao = new PostDao(DataSourceSearcher.getInstance().getDataSource());
-        MapperPost mapperPost = new MapperPost();
-        Utils utils = new Utils();
+    public Object execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String idParam = request.getParameter("id");
+        PostViewDto post = null;
 
-        User loggedUser = utils.getUserFromSession(req);
-        String username = req.getParameter("username");
+        try {
+            User user = utils.getUserFromSession(request);
+            if (user == null) {
+                return "/src/views/feed.jsp";
+            }
 
-        if (username == null && loggedUser != null) {
-            username = loggedUser.getUsername();
-        }
+            if (idParam != null && !idParam.isEmpty()) {
+                int id = Integer.parseInt(idParam);
+                Optional<Post> optionalPost = postDao.getPostById(id);
 
-        if (username != null && !username.isEmpty()) {
-            List<PostListDto> posts = mapperPost.mapPostListEntityToPostListDto(
-                    postDao.getPostsByUsername(username),
-                    postDao,
-                    loggedUser.getId()
-            );
+                if (optionalPost.isPresent()) {
+                    post = mapperPost.mapResultSetToPostViewDto(optionalPost.get(), postDao, user.getId());
+                }
+            }
 
-            req.setAttribute("posts", posts);
-            req.setAttribute("user", loggedUser);
-            return "profile.jsp";
-        } else {
-            return "feed.jsp";
+            if (post != null) {
+                request.setAttribute("post", post);
+                return "/src/views/threads.jsp";
+            } else {
+                return "/src/views/feed.jsp";
+            }
+        } catch (SQLException | NumberFormatException e) {
+            return "/src/views/feed.jsp";
         }
     }
 }
